baa5f099db0008acd22e50a9e22ad15d7d746a0d
vjcitn <stvjc@channing.harvard.edu>
2022-03-27 06:27:58 -0400

    prints out strings for google sheet and emails

--- R/getemail.R
+++ R/getemail.R
@@ -13,19 +13,50 @@ getDevEmail =
 #' @param condition character(1) "ERROR" or "WARNINGS",
 #' @param phase character(1) "buildsrc", "checksrc", "install", "buildbin"
 #' @param version character(1) "release" (default) or "devel"
+#' @param dump logical(1) if TRUE (default) produce pastable text
 #' @examples
 #' getRangeTable("ReactomePA", "SLqPCR")
 #' @export
-getRangeTable = function(start, end, condition="ERROR", phase="buildsrc", version="release") {
+getRangeTable = function(start, end, condition="ERROR", phase="buildsrc", version="release",
+    dump=TRUE) {
   ps = biocPkgRanges(start=start, end=end, condition=condition, phase=phase, version=version)
   nms = names(ps)
   em = sapply( nms, getDevEmail )
-  data.frame(pkg=names(em), email=unname(em))
+  lp = getLandingPage( nms, version=version )
+  ans = data.frame(pkg=names(em), email=unname(em), url=lp)
+  versionnum = "3.14"
+  if (version == "devel") versionnum = "3.15"
+  if (!dump) return(ans)
+  cat("subject:\n----\n")
+  cat(sprintf("Your Bioconductor package is in %s state in %s phase in version %s\n", condition, phase, versionnum))
+  cat("\n----\n")
+  writeLines(plzstr())
+  cat("\n----\n")
+  cat("package names\n----\n")
+  writeLines(ans$pkg)
+  cat("\n----\n")
+  cat("email\n----\n")
+  writeLines(ans$email)
+  cat("\n----\n")
+  cat("url\n----\n")
+  writeLines(paste(ans$pkg, ans$url))
+  cat("\n----\n")
+  invisible(ans)
 }
 
+plzstr = function() "Please find your contributed package below, and check the given link to the
+landing page.  Among the badges you will find some red ones.  Please correct the
+errors.  Thanks!"
 
 
 #> biocPkgRanges("ReactomePA", "SLqPCR") -> bb
 #> args(biocPkgRanges)
 #function (start, end, condition = c("ERROR", "WARNINGS"), phase = "buildsrc", 
 #    version = c("devel", "release")) 
+
+#' make URL for landing page
+#' @param x character(1) package name
+#' @param version character(1) "release" or "devel"
+getLandingPage = function(x, version="release") {
+  sprintf("https://bioconductor.org/packages/%s/bioc/html/%s.html", version, x)
+}
